1655bcf8fb1d8b2ffad27c3d782eceb70af2965c
Mohamed <mohamedibrahimahmedmahdi@gmail.com>
2024-08-03 23:23:24 +0300

    handle search header and fixed header

--- main.js
+++ main.js
@@ -3,6 +3,14 @@
 // ---------------------------------------
 let navBtnBurger = document.querySelector(".nav-phone")
 let navToDisplay = document.querySelector(".header nav ul")
+let searchFeild = document.querySelector(".header .search-feild")
+let searchBtn = document.querySelector(".header .controls .search > i")
 navBtnBurger.onclick = function () {
   navToDisplay.classList.toggle("current-screen")
+  searchFeild.classList.remove("active-search")
+}
+searchBtn.onclick = function () {
+  searchFeild.focus();
+  searchFeild.classList.toggle("active-search")
+  navToDisplay.classList.remove("current-screen")
 }
