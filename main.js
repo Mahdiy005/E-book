842b94a6f415559e750614b13509ec5e64073229
Mohamed <mohamedibrahimahmedmahdi@gmail.com>
2024-08-04 18:07:41 +0300

    end header

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // ---------------------------------------
 // handle navigation links in phone screen
 // ---------------------------------------
-let navBtnBurger = document.querySelector(".nav-phone")
-let navToDisplay = document.querySelector(".header nav ul")
-let searchFeild = document.querySelector(".header .search-feild")
-let searchBtn = document.querySelector(".header .controls .search > i")
+let navBtnBurger = document.querySelector(".nav-phone");
+let navToDisplay = document.querySelector(".header nav ul");
+let searchFeild = document.querySelector(".header .search-feild");
+let searchBtn = document.querySelector(".header .controls .search > i");
 navBtnBurger.onclick = function () {
-  navToDisplay.classList.toggle("current-screen")
-  searchFeild.classList.remove("active-search")
-}
+  navToDisplay.classList.toggle("current-screen");
+  searchFeild.classList.remove("active-search");
+};
 searchBtn.onclick = function () {
   searchFeild.focus();
-  searchFeild.classList.toggle("active-search")
-  navToDisplay.classList.remove("current-screen")
-}
+  searchFeild.classList.toggle("active-search");
+  navToDisplay.classList.remove("current-screen");
+};
